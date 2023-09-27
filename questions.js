97ab14b1e80d9bac62809b4a873067ac6a450844
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-09-27 11:48:48 -0300

    upload arquivos

---
+++ questions.js
@@ -0,0 +1,134 @@
+let questions = [
+    {
+        question: 'De quem é a famosa frase “Penso, logo existo”?',
+        options: [
+            'Platão',
+            'Galileu Galilei',
+            'Descartes',
+            'Sócrates',
+            'Francis Bacon'
+        ],
+        answer: 2
+    },
+    {
+        question: 'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+        options: [
+            'Jânio Quadros',
+            'Jacinto Anjos',
+            'Getúlio Vargas',
+            'João Figueiredo',
+            'João Goulart'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Qual o grupo em que todas as palavras foram escritas corretamente?',
+        options: [
+            'Asterístico, beneficiente, meteorologia, entertido',
+            'Asterisco, beneficente, meteorologia, entretido',
+            'Asterisco, beneficente, metereologia, entretido',
+            'Asterístico, beneficiente, metereologia, entretido',
+            'Asterisco, beneficiente, metereologia, entretido'
+        ],
+        answer: 1
+    },
+    {
+        question: 'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+        options: [
+            '8',
+            '10',
+            '9',
+            '5',
+            '7'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Quanto tempo a luz do Sol demora para chegar à Terra?',
+        options: [
+            '12 minutos',
+            '1 dia',
+            '12 horas',
+            '8 minutos',
+            '12 segundos'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual a nacionalidade de Che Guevara?',
+        options: [
+            'Cubana',
+            'Peruana',
+            'Panamenha',
+            'Boliviana',
+            'Argentina'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Qual a altura da rede de vôlei nos jogos masculino e feminino?',
+        options: [
+            '2,4 para ambos',
+            '2,5 m e 2,0 m',
+            '1,8 m e 1,5 m',
+            '2,45 m e 2,15 m',
+            '2,43 m e 2,24 m'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Em que ordem surgiram os modelos atômicos?',
+        options: [
+            'Thomson, Dalton, Rutherford, Rutherford-Bohr',
+            'Rutherford-Bohr, Rutherford, Thomson, Dalton',
+            'Dalton, Rutherford-Bohr, Thomson, Rutherford',
+            'Dalton, Thomson, Rutherford-Bohr, Rutherford',
+            'Dalton, Thomson, Rutherford, Rutherford-Bohr'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Qual das alternativas abaixo apenas contêm classes de palavras?',
+        options: [
+            'Vogais, semivogais e consoantes',
+            'Artigo, verbo transitivo e verbo intransitivo',
+            'Fonologia, Morfologia e Sintaxe',
+            'Hiatos, ditongos e tritongos',
+            'Substantivo, verbo e preposição'
+        ],
+        answer: 4
+    },
+    {
+        question: 'Qual a montanha mais alta do Brasil?',
+        options: [
+            'Pico da Neblina',
+            'Pico Paraná',
+            'Monte Roraima',
+            'Pico Maior de Friburgo',
+            'Pico da Bandeira'
+        ],
+        answer: 0
+    },
+    {
+        question: 'Qual a velocidade da luz?',
+        options: [
+            '300 000 000 metros por segundo (m/s)',
+            '150 000 000 metros por segundo (m/s)',
+            '199 792 458 metros por segundo (m/s)',
+            '299 792 458 metros por segundo (m/s)',
+            '30 000 000 metros por segundo (m/s)'
+        ],
+        answer: 3
+    },
+    {
+        question: 'Qual destes países é transcontinental?',
+        options: [
+            'Rússia',
+            'Filipinas',
+            'Marrocos',
+            'Groenlândia',
+            'Tanzânia'
+        ],
+        answer: 0
+    },
+];
---
+++ script.js
@@ -0,0 +1,93 @@
+// Initial Data
+let currentQuestion = 0;
+let correctAnswers = 0;
+let scoreArea =   document.querySelector('.scoreArea')
+showQuestion();
+
+// Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
+// Functions
+function showQuestion() {
+    if(questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        });
+    } else {
+        finishQuiz();
+    }
+}
+
+function optionClickEvent(e) {
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickedOption) {
+        correctAnswers++;
+    }
+
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuiz() {
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if(points < 30) {
+      document.querySelector('.scoreText1').innerHTML = 'Precisa Melhorar :(';
+      document.querySelector('.scorePct').style.color = '#FF0000';
+      document.querySelector('.prizeImage').style.display = 'none';
+      document.querySelector('.badImage').style.display = 'flex';
+      document.querySelector('.badGif').style.display = 'flex';
+      document.querySelector('.prizeGif').style.display = 'none';
+      document.querySelector('.goodImage').style.display = 'none';
+      document.querySelector('.goodGif').style.display = 'none';
+    } else if(points >= 30 && points < 70) {
+      document.querySelector('.scoreText1').innerHTML = 'Muito bom';
+      document.querySelector('.scorePct').style.color = '#FFFF00';
+      document.querySelector('.prizeImage').style.display = 'none';
+      document.querySelector('.goodImage').style.display = 'flex';
+      document.querySelector('.goodGif').style.display = 'flex';
+      document.querySelector('.prizeGif').style.display = 'none';
+      document.querySelector('.badImage').style.display = 'none';
+      document.querySelector('.badGif').style.display = 'none';
+    } else if(points >= 70) {
+      document.querySelector('.scoreText1').innerHTML = 'Parabéns!!';
+      document.querySelector('.scorePct').style.color = '#0D630D';
+      document.querySelector('.prizeImage').style.display = 'flex';
+      document.querySelector('.prizeGif').style.display = 'flex';
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+
+    document.querySelector('.scoreArea').style.display = 'flex';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+}
+
+
+function resetEvent() {
+  scoreArea.classList.add('animate__animated', 'animate__zoomOutDown');
+  scoreArea.style.setProperty('--animate-duration', '2s');
+      scoreArea.addEventListener('animationend', () => {
+        correctAnswers = 0;
+        currentQuestion = 0;
+        showQuestion();
+        window.location.reload(true);
+      });
+}
